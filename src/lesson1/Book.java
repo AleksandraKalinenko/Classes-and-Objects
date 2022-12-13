e9ae7837c3c048132a08d763a0edba08edb7423e
79613 <kalinenko2000@gmail.com>
2022-12-13 20:20:24 +0300

    First Commit

--- src/lesson1/Book.java
+++ src/lesson1/Book.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -25,5 +27,23 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Книга:" + '\n' + "Название: " + name + " " + author.toString() + " " + "Год издания: " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
     }
 }
--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -4,20 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Author levTolstoy = new Author("Лев", "Толстой");
-//        System.out.println(levTolstoy.getName() + " " + levTolstoy.getSurname());
 
         Author aleksandrPushkin = new Author("Александр", "Пушкин");
-//        System.out.println(aleksandrPushkin.getName() + " " + aleksandrPushkin.getSurname());
+        System.out.println(aleksandrPushkin);
 
         Book warAndPeace = new Book("Война и мир", levTolstoy, 1863);
-        System.out.println(warAndPeace.getName() + "\n" + warAndPeace.getAuthor().getName() + " "
-                + warAndPeace.getAuthor().getSurname() + "\n" + warAndPeace.getYear());
+        System.out.println(warAndPeace);
 
         Book evgenyOnegin = new Book("Евгений Онегин", aleksandrPushkin, 1834);
-        System.out.println(evgenyOnegin.getName() + "\n" + evgenyOnegin.getAuthor().getName() + " "
-                + evgenyOnegin.getAuthor().getSurname() + "\n" + evgenyOnegin.getYear());
 
         evgenyOnegin.setYear(1833);
-        System.out.println("evgenyOnegin.getName = " + evgenyOnegin.getYear());
     }
 }
